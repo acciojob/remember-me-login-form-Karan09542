fe87a9182b65f13f3c77d8b3169edd989d7c795c
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-09-29 20:37:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const checkboxInput = document.getElementById("checkbox");
+const submitBtn = document.getElementById("submit");
+const existing = document.getElementById("existing");
+
+existing.addEventListener("click", () => {
+	const userCredential = JSON.parse(localStorage.getItem("userCredential"));
+	alert(`Logged in as ${userCredential.username}`)
+})
+
+window.onload = function(){
+	const userCredential = localStorage.getItem("userCredential");
+	if(userCredential){
+		existing.hidden = false;
+	}
+}
+
+function onSubmit(e){
+	// e.preventDefault();
+	const username = usernameInput.value;
+	const password = passwordInput.value;
+	const isRemember = checkboxInput.checked;
+
+	const credential = {username, password, isRemember}
+
+	if(!isRemember){
+		localStorage.removeItem("userCredential")
+		alert("credentials are not stored")
+	}else {
+		localStorage.setItem("userCredential",JSON.stringify(credential))
+		alert("credentials are stored in localStorage")
+	}
+	
+}
